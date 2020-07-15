fd57fa04b0c32f87f77e31b8e7c3cbac659a0148
Xiang.Wang10@geely.com <fuckjltest>
2020-07-15 13:46:02 +0800

    add tree

--- source/src/main/java/com/bob/source/collection/ListClass.java
+++ source/src/main/java/com/bob/source/collection/ListClass.java
@@ -2,58 +2,99 @@ package com.bob.source.collection;
 
 public class ListClass {
 
-    /**
-     * 基于数组实现的有界队列，一旦创建后容量不可更改
-     */
-    class ArrayBlockingQueue {
+  interface Iterable<T> {
 
-    }
+  }
 
-    /**
-     * 基于链表的有界阻塞队列，添加跟获取是两个不同的锁，队列默认长度是 Integer.MAX_VALUE
-     */
-    class LinkedBlockingQueue {
+  interface Collection<E> extends Iterable<E> {
 
-    }
+  }
+
+  interface Queue<E> extends Collection<E> {
 
     /**
-     * 基于数组，支持优先级使用二叉堆数据结构实现的无界阻塞队列
+     * 队列满时会返回 false
      */
-    class PriorityBlockingQueue {
-
-    }
+    boolean offer(E e);
 
     /**
-     * 支持延时获取元素的无界阻塞队列
+     * 队列满时会抛出异常，一般实现中会转而调用 offer
      */
-    class DelayQueue {
-
-    }
+    boolean add(E e);
 
     /**
-     * 容量为 0 ，一个添加操作后必须等待一个获取操作才可以继续添加，CPU 自旋等待消费者取走元素
+     * 队列空时抛出异常，会将元素从队列中移除
      */
-    class SynchronousQueue {
-
-    }
+    E remove();
 
     /**
-     * 双向链表组成的双向阻队列，无界
+     * 队列空时返回 null，会将元素从队列中移除
      */
-    class LinkedBlockingDeque {
-
-    }
+    E poll();
 
     /**
-     * 一个由链表组成的无界阻塞队列
+     * 队列空时抛出异常，元素不会从队列中移除
      */
-    class LinkedTransferQueue {
+    E element();
 
-        /**
-         * 如果当前有消费线程正在获取元素则把该元素直接传递给消费线程否则加入到队列中直到该元素被消费才返回
-         */
-        void transfer() {
+    /**
+     * 队列空时返回 null，元素不会从队列中移除
+     */
+    E peek();
+  }
 
-        }
+  /**
+   * 基于数组实现的有界队列，一旦创建后容量不可更改
+   */
+  class ArrayBlockingQueue {
+
+  }
+
+  /**
+   * 基于链表的有界阻塞队列，添加跟获取是两个不同的锁，队列默认长度是 Integer.MAX_VALUE
+   */
+  class LinkedBlockingQueue {
+
+  }
+
+  /**
+   * 基于数组，支持优先级使用二叉堆数据结构实现的无界阻塞队列
+   */
+  class PriorityBlockingQueue {
+
+  }
+
+  /**
+   * 支持延时获取元素的无界阻塞队列
+   */
+  class DelayQueue {
+
+  }
+
+  /**
+   * 容量为 0 ，一个添加操作后必须等待一个获取操作才可以继续添加，CPU 自旋等待消费者取走元素
+   */
+  class SynchronousQueue {
+
+  }
+
+  /**
+   * 双向链表组成的双向阻队列，无界
+   */
+  class LinkedBlockingDeque {
+
+  }
+
+  /**
+   * 一个由链表组成的无界阻塞队列
+   */
+  class LinkedTransferQueue {
+
+    /**
+     * 如果当前有消费线程正在获取元素则把该元素直接传递给消费线程否则加入到队列中直到该元素被消费才返回
+     */
+    void transfer() {
+
     }
+  }
 }
---
+++ source/src/main/java/com/bob/source/tree/TreeClass.java
@@ -0,0 +1,109 @@
+package com.bob.source.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringJoiner;
+
+/**
+ * 〈〉
+ *
+ * @author wangxiang
+ * @create 2020/7/13
+ */
+public class TreeClass {
+
+  public static void main(String[] args) {
+
+    TreeClass treeClass = new TreeClass();
+    BinaryTree root = treeClass.buildBinaryTree(5);
+
+    treeClass.iterateBinaryTree(root);
+  }
+
+  private void iterateBinaryTree(BinaryTree tree) {
+
+    System.out.println(tree);
+    Queue<BinaryTree> queue = new LinkedList<>();
+    BinaryTree currentNode = tree;
+    queue.offer(currentNode);
+    while (!queue.isEmpty()) {
+      BinaryTree temp = queue.poll();
+      System.out.println(temp.getValue());
+      if(null != temp.getLeft()) {
+        queue.offer(temp.getLeft());
+      }
+      if(null != temp.getRight()) {
+        queue.offer(temp.getRight());
+      }
+    }
+  }
+
+  private BinaryTree buildBinaryTree(int length) {
+
+    List<BinaryTree> treeList = new ArrayList<>();
+    for (int i = 0; i < length; i++) {
+      treeList.add(new BinaryTree(i));
+    }
+    BinaryTree root = treeList.get(0);
+    for (int index = 0; index < treeList.size() / 2; index++) {
+      if (2 * index + 2 < treeList.size()) {
+        treeList.get(index).setRight(treeList.get(2 * index + 2));
+      }
+      treeList.get(index).setLeft(treeList.get(2 * index + 1));
+
+    }
+    return root;
+  }
+
+  /**
+   * 二叉树
+   */
+  static class BinaryTree {
+
+    private int value;
+    private BinaryTree left;
+    private BinaryTree right;
+
+    public BinaryTree() {
+    }
+
+    public BinaryTree(int value) {
+      this.value = value;
+    }
+
+    public int getValue() {
+      return value;
+    }
+
+    public void setValue(int value) {
+      this.value = value;
+    }
+
+    public BinaryTree getLeft() {
+      return left;
+    }
+
+    public void setLeft(BinaryTree left) {
+      this.left = left;
+    }
+
+    public BinaryTree getRight() {
+      return right;
+    }
+
+    public void setRight(BinaryTree right) {
+      this.right = right;
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", BinaryTree.class.getSimpleName() + "[", "]")
+          .add("value=" + value)
+          .add("left=" + left)
+          .add("right=" + right)
+          .toString();
+    }
+  }
+}
