e76819ae733b8112d09eab6be3ca810920efefc5
王相 <xiang.wang10@geely.com>
2021-03-12 10:36:12 +0800

    add sh

---
+++ bash/mvn-deploy.sh
@@ -0,0 +1,8 @@
+mvn deploy:deploy-file \
+-DgroupId=taobao \
+-DartifactId=taobao \
+-Dversion=0.0.1-SNAPSHOT \
+-Dpackaging=jar \
+-Dfile=/Users/wangxiang/Downloads/dingtalk/taobao.jar \
+-Durl=http://gg-maven.geely.com/repository/maven-snapshots/ \
+-DrepositoryId=maven-snapshots
